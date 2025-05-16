1b66fa9c3ae6229ff0bfd3d48c1ea2a87a29c7f5
noxware <7684329+noxware@users.noreply.github.com>
2025-05-16 02:23:37 -0300

    single file + minimal deps + upgrade

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-mod app;
-
 fn main() -> eframe::Result {
     env_logger::init();
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
         ..Default::default()
     };
-    eframe::run_native(
-        "App",
-        options,
-        Box::new(|cc| {
-            egui_extras::install_image_loaders(&cc.egui_ctx);
-            Ok(Box::<app::App>::default())
-        }),
-    )
+    eframe::run_native("app", options, Box::new(|_cc| Ok(Box::new(App::new()))))
+}
+
+pub struct App {
+    name: String,
+    age: u32,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            name: "Arthur".into(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("Age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
 }
