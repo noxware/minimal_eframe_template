97b3521d4e93d57e0b99e38b95c713f42c15d26a
noxware <7684329+noxware@users.noreply.github.com>
2026-08-23 20:02:25 -0300

    bump

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,8 @@ impl App {
 }
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ui, |ui| {
             ui.heading("My egui Application");
             ui.horizontal(|ui| {
                 ui.label("Your name: ");
